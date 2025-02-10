007d670479983c19bd098daffd84b1460b23cb9f
Julian <songsterr2013@gmail.com>
2025-02-10 16:55:18 +0800

    discography added

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ def main():
         return
 
     # 方法1: 輸入關鍵字 (單次搜索)
-    # value = input("請輸入關鍵字 (例如: Chet Baker vinyl, Bill Evans vinyl): ")
+    #jazz_giant = [input("請輸入關鍵字 (例如: Chet Baker vinyl, Bill Evans vinyl): ")]
 
     # 方法2: 讀json檔
     # config = load_config("config.json")
     # jazz_giant = get_config_value(config, "jazz_giant")
 
-    # 方法3: 目前先直接在裡面的list中修改就好
+    # 方法3: for開發用，直接在裡面的list中修改就好
     '''jazz_giant = ['Bill Evans', 'Chet Baker', 'Miles Davis',
                       'John Coltrane', 'Keith Jarrett', 'Stan Getz',
                       'Louis Armstrong', 'Duke Ellington', 'Dave Brubeck',
@@ -36,29 +36,19 @@ def main():
                       'Billie Holiday', 'Jim Hall', 'Sarah Vaughan',
                       ]'''
 
-    jazz_giant = ['Bill Evans', 'Chet Baker', 'Miles Davis',
-                  'John Coltrane', 'Keith Jarrett', 'Stan Getz',
-                  'Louis Armstrong', 'Duke Ellington', 'Dave Brubeck',
-                  'Wes Montgomery', 'Ella Fitzgerald', 'Charlie Parker',
-                  'Herbie Hancock', 'Cannonball Adderley', 'Erroll Garner',
-                  'Frank Sinatra', 'Chick Corea', 'Gerry Mulligan',
-                  'Charles Mingus', 'Oscar Peterson', 'Joe Pass',
-                  'Ornette Coleman', 'Sonny Rollins', 'Thelonious Monk',
-                  'Pat Metheny', 'Antonio Carlos Jobim', 'Lee Morgan',
-                  'Billie Holiday', 'Jim Hall', 'Sarah Vaughan',
-                  ]
+    jazz_giant =['Bill Evans']
 
     for name in jazz_giant:
         update_config(config_path=config_file, key=key, value=name + ' ' + 'vinyl')
 
         print("執行 Ebay.py 取得data...")
-        #subprocess.run([python_exe, py1])
+        subprocess.run([python_exe, py1])
 
         print("執行 DataCleaning.py 來整理數據並生成 Excel...")
         subprocess.run([python_exe, py2])
 
         print("執行 CreateTableau.py 來基於Tableau模板進行複製...")
-        #subprocess.run([python_exe, py3])
+        subprocess.run([python_exe, py3])
 
 if __name__ == "__main__":
 
--- util.py
+++ util.py
@@ -1,6 +1,7 @@
 import json
 import os
 import logging
+import pandas as pd
 
 def setup_logging(log_filename='api_calls.log'):
     logging.basicConfig(
@@ -57,3 +58,41 @@ def get_config_value(config, key):
 def make_folder(dir_name):
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
+
+# make all excel in one, for tableau using
+def combine_excel_files(base_path, output_file):
+    # make an empty df
+    combined_df = pd.DataFrame()
+
+    # traversal base_path
+    for folder_name in os.listdir(base_path):
+        folder_path = os.path.join(base_path, folder_name)
+
+        # check if file start with 'data_n'
+        if os.path.isdir(folder_path) and folder_name.startswith('data_'):
+            folder_id = folder_name.split('_', 1)[1] if '_' in folder_name else folder_name
+
+            for file_name in os.listdir(folder_path):
+                if file_name.startswith(folder_id) and file_name.endswith('.xlsx'):
+                    file_path = os.path.join(folder_path, file_name)
+
+                    df = pd.read_excel(file_path)
+                    artist_name = folder_id.replace("_", " ")
+                    df['藝人名稱'] = artist_name
+                    combined_df = pd.concat([combined_df, df], ignore_index=True)
+                    print(f'{artist_name}相關DATA已加入')
+
+    if '藝人名稱' in combined_df.columns:
+        cols = ['藝人名稱'] + [col for col in combined_df.columns if col != '藝人名稱']
+        combined_df = combined_df[cols]
+
+    combined_df.to_excel(output_file, index=False)
+
+    return combined_df
+
+'''if __name__ == '__main__':
+    base_path = './data'
+    output_file = './all_artist_data.xlsx'
+
+    # 呼叫工具函式
+    combine_excel_files(base_path, output_file)'''
